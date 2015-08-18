1735addb11d0a08964b0e99156714ae3f96c920e
mchow <mchow@users.noreply.github.com>
2015-08-18 15:23:04 -0700

    decorate retry with metrics

--- app/examples/s_requests.py
+++ app/examples/s_requests.py
@@ -1,18 +1,30 @@
 import requests
 from retrying import retry
+from requests import ConnectionError, HTTPError
 
+def decroate_retry(func):
+  @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=5)
+  def retry_metrics(*args, **kwargs):
+    try:
+      response = func(*args, **kwargs)
+      response.raise_for_status()
+    except ConnectionError as e:
+      print("ConnectionError:", e.message)
+      #log metrics
+      raise
+    except HTTPError as e:
+      print("HTTPError:", e.message)
+      #log metrics
+      raise
 
-@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=5)
+  return retry_metrics
+
+# @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=5)
+@decroate_retry
 def test_500_retry():
   url = "https://httpbin.org/status/500"
-
   response = requests.get(url)
-  try:
-    print("testing")
-    response.raise_for_status()
-  except requests.exceptions.HTTPError as e:
-    print('And you get an HTTPError: %s' % e.message)
-    raise
+  return response
 
 if __name__ == '__main__':
   print("hello")
